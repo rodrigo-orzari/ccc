35443400a54bece0d99be9b46814d6a36418f948
Rodrigo Orzari <rodrigo.orzari@outlook.com>
2026-06-04 17:00:31 -0700

    d

---
+++ debug_filters.ts
@@ -0,0 +1,3 @@
+import { buildPricingFilters } from './server.ts'; // wait server.ts does not export buildPricingFilters
+
+// I will just read server.ts and eval it, or better, modify server.ts temporarily to console.log it.
